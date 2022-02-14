aad905ca8f2220936dda93c73d1991bba305dd8d
grezar <grezar.dev@gmail.com>
2022-02-15 02:17:38 +0900

    Additional basic config

--- flake.nix
+++ flake.nix
@@ -17,7 +17,24 @@
             # Enable the OpenSSH daemon.
             services.openssh.enable = true;
             services.openssh.passwordAuthentication = true;
+            # FIXME: shouldn't be allowed
             services.openssh.permitRootLogin = "yes";
+
+            # My timezone
+            time.timeZone = "Asia/Tokyo";
+
+            # Don't require password for sudo.
+            security.sudo.wheelNeedsPassword = false;
+
+            # Select internationalisation properties.
+            i18n.defaultLocale = "ja_JP.UTF-8";
+
+            # Hostname.
+            networking.hostName = "dev";
+
+            # Disable the firewall since we're in a VM and we want to make it
+            # easy to visit stuff in here. We only use NAT networking anyways.
+            networking.firewall.enable = false;
           }
           ./hardware-configuration.nix
         ];
