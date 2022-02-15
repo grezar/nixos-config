67c754064f12d820a25fbb946865557f5888c781
grezar <grezar.dev@gmail.com>
2022-02-15 23:49:26 +0900

    git

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-21.11";
+      # We want home-manager to use the same set of nixpkgs as our system.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       vm-aarch64 = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
@@ -38,6 +43,11 @@
           }
           ./configuration.nix
           ./hardware-configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.grezar = import ./home.nix;
+          }
         ];
       };
     };
