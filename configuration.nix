934be8030807bc7284ce893da7c3c262835f1af5
grezar <grezar.dev@gmail.com>
2022-02-15 22:52:44 +0900

    normal user grezar

--- configuration.nix
+++ configuration.nix
@@ -7,4 +7,13 @@
     displayManager.defaultSession = "none+awesome";
     desktopManager.xterm.enable = false;
   };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.grezar = {
+    isNormalUser = true;
+    home = "/home/grezar";
+    description = "grezar";
+    shell = pkgs.zsh;
+    extraGroups = ["wheel"];
+  };
 }
