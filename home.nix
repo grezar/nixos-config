025674186d3c43d199fb59f5a2a87ac9d7930a34
grezar <grezar.dev@gmail.com>
2022-02-17 22:27:06 +0900

    install neovim

--- home.nix
+++ home.nix
@@ -26,4 +26,28 @@
       };
     };
   };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nord-vim
+      vim-airline
+      vim-airline-themes
+      vim-polyglot
+      vim-over
+      vim-trailing-whitespace
+      auto-pairs
+    ];
+    extraConfig = ''
+      syntax enable
+      set background=light
+      colorscheme nord
+
+      set number
+
+      inoremap <silent> jk <ESC>
+    '';
+  };
 }
