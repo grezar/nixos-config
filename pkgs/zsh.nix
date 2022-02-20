beba8f1494d3990096e2071911cdfb71ba7b6af0
grezar <grezar.dev@gmail.com>
2022-02-21 00:43:27 +0900

    Use zsh in nix-shell

--- pkgs/zsh.nix
+++ pkgs/zsh.nix
@@ -23,6 +23,16 @@
         src = lib.cleanSource ./p10k-config;
         file = ".p10k.zsh";
       }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.4.0";
+          sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+        };
+      }
     ];
   };
 }
