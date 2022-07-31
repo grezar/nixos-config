e71bdccc1a576c97a2aee378b849ca4592703aa3
grezar <grezar.dev@gmail.com>
2022-07-31 16:02:24 +0900

    scroll by mouse

--- users/grezar/tmux.nix
+++ users/grezar/tmux.nix
@@ -11,5 +11,10 @@
     plugins = with pkgs; [
       tmuxPlugins.nord
     ];
+    extraConfig = ''
+      set -g mouse on
+      bind-key -T edit-mode-vi WheelUpPane send-keys -X scroll-up
+      bind-key -T edit-mode-vi WheelDownPane send-keys -X scroll-down
+    '';
   };
 }
